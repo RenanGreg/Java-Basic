d0b73d8f50b24b7d3d900fac4fcf9ae0ce9f0672
Renan Gregório <145120433+RenanGreg@users.noreply.github.com>
2024-11-01 19:39:58 +0000

    novo

--- src/OlaMundoJavaFX/FXMLDocumentController.java
+++ src/OlaMundoJavaFX/FXMLDocumentController.java
@@ -1,14 +1,26 @@
 import java.awt.Button;
 import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FXMLDocumentController 
-             implements Initializable { 
-    
-    private Button bntClick; 
-    private Label lblMensagem; 
-    private void clicouBotao (){ 
-      lblMensagem.setText("Ola, Mundo"); 
+public class FXMLDocumentController {
+
+    private Button bntClick;
+    private Label lblMensagem;
+
+    public FXMLDocumentController() {
+        bntClick = new Button("Clique aqui");
+        lblMensagem = new Label("Mensagem inicial");
+
+        bntClick.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clicouBotao();
+            }
+        });
     }
-  } 
 
-
+    private void clicouBotao() {
+        lblMensagem.setText("Olá, Mundo");
+    }
+}
