a221a9e762abc1fad7ca51cf7c8a203e67a9a5d3
Renan Gregório <145120433+RenanGreg@users.noreply.github.com>
2024-11-01 18:13:15 +0000

    swing-javaFX

---
+++ src/OlaMundoJavaFX/FXMLDocumentController.java
@@ -0,0 +1,14 @@
+import java.awt.Button;
+import java.awt.Label;
+
+public class FXMLDocumentController 
+             implements Initializable { 
+    
+    private Button bntClick; 
+    private Label lblMensagem; 
+    private void clicouBotao (){ 
+      lblMensagem.setText("Ola, Mundo"); 
+    }
+  }  
+
+
---
+++ src/main/OlaMundoSwing/TelaSwing.java
@@ -0,0 +1,7 @@
+public class TelaSwing extends javax.swing.JFrame{
+  private javax.swing.JButton btnClick; 
+  private javax.swing.JLabel lblMensagem; 
+  private void bntClickActionPerformed() {
+    lblMensagem.setText("Ola, Mundo"); 
+  }
+} 
